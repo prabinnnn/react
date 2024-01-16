68db1c0caefd3da920fbb87a8711ef440a22eb0e
prabinnnn <prabinbhandari120@gmail.com>
2024-01-16 17:49:27 +0545

    add node2

--- node2.js
+++ node2.js
@@ -1,4 +1,3 @@
-const { hash } = require("bcrypt");
 const bcryptjs = require("bcryptjs");
 const encript = (string) => {
   return bcryptjs.hashSync(string, 10);
@@ -7,5 +6,5 @@ const hasedpw = encript("password");
 const compare = (string, hash) => {
   return bcryptjs.compareSync(string, hash);
 };
-const prabin = compare("ihaswhded", hasedpw);
+const prabin = compare("password", hasedpw);
 console.log({ prabin });
