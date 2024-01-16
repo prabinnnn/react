dae289a299c4f0b068c916068f8da8be7e3ca695
prabinnnn <prabinbhandari120@gmail.com>
2024-01-16 18:00:12 +0545

    add node2

--- node2.js
+++ node2.js
@@ -6,5 +6,4 @@ const hasedpw = encript("password");
 const compare = (string, hash) => {
   return bcryptjs.compareSync(string, hash);
 };
-const prabin = compare("password", hasedpw);
-console.log({ prabin });
+console.log(compare("password", hasedpw));
