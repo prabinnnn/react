693c89892871e5b4cc6fddd66ad5fccff116387b
prabinnnn <prabinbhandari120@gmail.com>
2024-01-17 16:30:32 +0545

    add node5

--- node5.js
+++ node5.js
@@ -1,5 +1,7 @@
 const CC = require("currency-converter-lt");
+
 let currencyConverter = new CC();
+
 currencyConverter
   .from("USD")
   .to("GBP")
@@ -7,4 +9,7 @@ currencyConverter
   .convert()
   .then((response) => {
     console.log(response);
+  })
+  .catch((error) => {
+    console.error(error);
   });
